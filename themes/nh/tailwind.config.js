94c427b01363fc54f09b9b56358a148ad8cb7296
James Acklin <james@tlon.io>
2022-12-04 12:31:33 -0500

    base tailwind theme

--- themes/nh/tailwind.config.js
+++ themes/nh/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
+    fontFamily: {
+      sans: ['Arial', 'sans-serif'],
+      serif: ['Times New Roman', 'serif']
+    },
     extend: {},
   },
   plugins: [],
-};
+}
